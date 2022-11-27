371d74e163ffb51a9b631554e73bf138f4593ec5
olawalemayor <olawalemayor90@gmail.com>
2022-11-27 21:40:34 +0100

    added private and public keys

--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -1,11 +1,12 @@
 import jwt from "jsonwebtoken";
 
-const secretKey = process.env.JWT_SECRET_KEY as jwt.Secret;
+const privateKey = process.env.JWT_PRIVATE_KEY as jwt.Secret;
+const publicKey = process.env.JWT_PUBLIC_KEY as jwt.Secret;
 
 export const signJwt = (payload: jwt.JwtPayload) => {
-  return jwt.sign(payload, secretKey);
+  return jwt.sign(payload, privateKey, { expiresIn: "24h" });
 };
 
 export const verifyJwt = (token: string) => {
-  return jwt.verify(token, secretKey);
+  return jwt.verify(token, publicKey);
 };
