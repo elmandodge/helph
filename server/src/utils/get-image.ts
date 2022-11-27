72094093a5fcdc5dc00864b48f1b7cfc288030ae
olawalemayor <olawalemayor90@gmail.com>
2022-11-27 21:49:22 +0100

    moved server to another branch

--- server/src/utils/get-image.ts
+++
@@ -1,7 +0,0 @@
-export const getImage = async (imageFile: any) => {
-  // Stores the image to cloud
-  // Gets the image url from cloud
-  // Returns the image URL
-
-  return "https://sampleImage.png";
-};
